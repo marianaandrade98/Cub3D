f50d685f99c284c7cbd1b3a92128a2a738e52455
Mariana <mandrade@c1r21s7.42lisboa.com>
2021-05-04 17:15:42 +0100

    red square

--- cub/main.c
+++ cub/main.c
@@ -12,8 +12,17 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
     char    *dst;
 
-    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-    *(unsigned int*)dst = color;
+	// y = 0;
+	// while (++y < 50)
+	// 	continue ;
+	for(y = 0; y < 50; ++y)
+	// x = 0;
+	// while (++x < 50)
+	for(x = 0; x < 50; ++x)
+	{
+    	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    	*(unsigned int*)dst = color;
+	}
 }
 
 int	main(void)
@@ -27,7 +36,9 @@ int	main(void)
 	img.img = mlx_new_image(mlx, 1920, 1080);
     img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
                                  &img.endian);
+
     my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+
     mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
