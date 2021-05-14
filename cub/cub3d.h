f648ef6c1c5a3d39cdb3486c5933f0d2af83a9e9
Mariana <mandrade@c1r12s6.42lisboa.com>
2021-05-14 18:46:15 +0100

    add defines

--- cub/cub3d.h
+++ cub/cub3d.h
@@ -6,6 +6,27 @@
 # include "mlx/mlx.h"
 # include "get_next_line/get_next_line.h"
 
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+# define ERROR 1
+
+# define SPEED 15
+# define TURN 0.05
+
+# define KEY_A 0
+# define KEY_S 1
+# define KEY_D 2
+# define KEY_W 13
+
+# define KEY_LEFT 123
+# define KEY_RIGHT 124
+# define KEY_DOWN 125
+# define KEY_UP 126
+
+# define KEY_ESC 53
+
+
 typedef struct  s_data {
     void        *img;
     char        *addr;
